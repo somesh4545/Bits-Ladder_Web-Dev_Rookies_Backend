b26be4dfbed30fab617175d107431028994e33d5
Somesh Somani <57597556+somesh4545@users.noreply.github.com>
2023-02-12 17:52:28 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -14,13 +14,7 @@ async function sendNotification(
   if (sendSMS == true) {
     const worker = await Worker.findOne({ _id: userID }).select("phone_no");
 
-    const url =
-      " https://www.fast2sms.com/dev/bulkV2?authorization=JCDv7FSl1P5IgxmAz0kfw64QRbVchT8OdUMetZ2YXKEHiWryGaNdhi0jmZU4QVLGtlsJWDr6eO7KuYHX&route=v3&sender_id=FTWSMS&message=" +
-      title +
-      "\n" +
-      description +
-      "&language=english&flash=0&numbers=" +
-      worker.phone_no;
+    const url = "Your api url to send the sms";
 
     axios
       .get(url)
